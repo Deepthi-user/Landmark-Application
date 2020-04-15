6cf4e890e476c390a1ababf989cb6bd72c614f3f
Deepthi <deepthinravi@gmail.com>
2020-04-15 09:47:22 +0530

    adding property file

--- src/main/java/com/project/Smartparking_landmarks/constant/Constant.java
+++
@@ -1,6 +0,0 @@
-package com.project.Smartparking_landmarks.constant;
-
-public interface Constant {
-    public static final String REGEX_FOR_LATITUDE = "^-?([1]?[1-7][1-9]|[1-8]?[1-9]|[1-9])\\.{1}\\d{1,6}";
-    public static final String REGEX_FOR_LONGITUDE = "^-?([1]?[1-7][1-9]|[1]?[1-8][0]|[1-9]?[0-9])\\.{1}\\d{1,6}";
-}
